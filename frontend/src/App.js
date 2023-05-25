81db0170be11e26aded0ad78d187561c43bd14c1
Joe Webster <joseph.scott.webster@gmail.com>
2023-05-25 16:26:48 -0400

    Implement basic frontend routing
    Co-authored-by: Joe Webster <joseph.scott.webster@gmail.com>
    Co-authored-by: Mat Sisserson <masisserson@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link to="/endpoints">Endpoints</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/feed" element={<h1>Feed</h1>} />
+        <Route path="/endpoints" element={<h1>Endpoints</h1>} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
