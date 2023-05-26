6d3cd4289afcbba218872f1726f884f97dcba65b
Joe Webster <joseph.scott.webster@gmail.com>
2023-05-26 17:36:11 -0400

    Click individual request to view request details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,6 @@ const RequestList = ({ requests }) => {
 
 const EndpointsPage = () => {
   const [endpoints, setEndpoints] = useState([]);
-  // const [selectedEndpoint, setSelectedEndpoint] = useState(null);
 
   useEffect(() => {
     const getAllEndpoints = async () => {
@@ -102,6 +101,8 @@ const EndpointList = ({ endpoints }) => {
 const EndpointDetailPage = () => {
   const { id } = useParams();
   const [requests, setRequests] = useState([]);
+  const [selectedRequest, setSelectedRequest] = useState(null);
+
 
   useEffect(() => {
     const getEndpointRequests = async () => {
@@ -116,25 +117,44 @@ const EndpointDetailPage = () => {
     }
 
     getEndpointRequests();
-  }, [])
+  }, [id])
+
+  const handleRequestClick = async (requestId) => {
+    const request = await axios.get(`http://localhost:3001/requests/${requestId}`);
+    setSelectedRequest(request.data);
+    console.log(request.data);
+  };
 
   return (
     <>
       <p>You endpoint is:</p>
       <h3>http://localhost:3001/endpoints/{id}</h3>
       <h2>Requests</h2>
-      {EndpointRequestSidebar({ requests })}
-
+      <EndpointDetailSidebar
+        requests={requests}
+        onRequestClick={handleRequestClick}
+        selectedRequestId={selectedRequest ? selectedRequest.id : null}
+      />
+      <EndpointDetailContent request={selectedRequest} />
     </>
   )
 }
 
-const EndpointRequestSidebar = ({ requests }) => {
-  console.log("in endpointrequestsidebar", requests);
-
+const EndpointDetailSidebar = ({ requests, onRequestClick, selectedRequestId }) => {
   const requestLineItems = () => {
     return requests.map(request => {
-      return <li key={request.id}>{JSON.stringify(request)}</li>
+      const { id, time_stamp, request_data } = request;
+      const method = request_data.method;
+      const timestamp = new Date(time_stamp).toLocaleString();
+
+      return (
+        <li
+          key={id}
+          onClick={() => onRequestClick(id)}
+          className={`request-item ${selectedRequestId === id ? 'request-selected' : ''}`}
+        >
+          {`${method} ${timestamp}`}
+        </li>)
     })
   }
 
@@ -145,11 +165,18 @@ const EndpointRequestSidebar = ({ requests }) => {
   )
 }
 
+const EndpointDetailContent = ({ request }) => {
+  return (
+    <div>
+      <h3>Request Detail</h3>
+      <p>{JSON.stringify(request)}</p>
+    </div>
+  )
+}
+
 // APP =========================================================================
 
 const App = () => {
-  const [selectedEndpoint, setSelectedEndpoint] = useState(null);
-
   return (
     <>
       <nav className='navigation'>
@@ -170,7 +197,6 @@ const App = () => {
         <Route path="/" element={<h2>Home</h2>} />
         <Route path="/feed" element={<FeedPage />} />
         <Route path="/endpoints" element={<EndpointsPage />} />
-        {/* <Route path="/endpoints" element={<EndpointsPage setSelectEndpoint={setSelectedEndpoint} />} /> */}
         <Route path="/endpoints/:id" element={<EndpointDetailPage />} />
       </Routes>
     </>
